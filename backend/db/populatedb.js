f724c3c09d56496be5bca15040fdd2fde33053ed
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2025-05-21 21:17:30 -0700

    created dropTables and main seed function for database population

--- backend/db/populatedb.js
+++ backend/db/populatedb.js
@@ -1,6 +1,23 @@
-const {
-    createUser,
-    
-} = require("./");
-const pool = require("./pool");
+const { createUser } = require("./");
+const { pool } = require("./index");
 
+async function dropTables() {
+    try {
+        console.log("Starting seed process");
+
+        // Drop all tables
+        await pool.query("DROP TABLE IF EXISTS users CASCADE;");
+    } catch (error) {
+        console.log("Error while dropping tables!");
+        throw error;
+    }
+}
+
+async function seed() {
+    try {
+        // Dropping tables
+        dropTables();
+    } catch (error) {
+
+    }
+}
