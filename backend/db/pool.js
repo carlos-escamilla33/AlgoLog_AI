c2bf847900027eace5f16198c1e5ecf6e73d8ecb
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2025-05-30 11:29:03 -0700

    updated pool.js for database connection

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -4,7 +4,15 @@ const pool = new Pool({
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    max: 10,
+    idleTimeoutmMillis: 30000,
+    connectionTimeoutMillis: 2000
 });
+
+pool.on("error", (err) => {
+    console.log("Unexpected error on idle client", err);
+    process.exit(-1);
+})
 
 module.exports = pool;
