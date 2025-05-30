ac47c143299028391e4b3429c291fc6f3f446178
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2025-05-30 11:26:41 -0700

    updated createUser in table functions

--- backend/db/tableFunctions/users.js
+++ backend/db/tableFunctions/users.js
@@ -1,10 +1,23 @@
 const { create } = require("domain");
 const pool = require("../pool");
+const bcrypt = require("bcrypt");
 
-async function createUser({username, password}) {
+async function createUser(userData, client) {
     try {
-        
+        const {username, email, password} = userData;
+
+        const saltRounds = 10;
+        const hashPassword = await bcrypt.hash(password, saltRounds);
+
+        const {rows:[user]} = await client.query(`
+            INSERT INTO users (username, email, password)
+            VALUES ($1, $2, $3)
+            RETURNING id, username, email, created_at
+        `, [username, email, hashPassword]);
+
+        return user;
     } catch (error) {
+        console.log(`Error creating user ${userData.username}`, error.message);
         throw error;
     }
 }
